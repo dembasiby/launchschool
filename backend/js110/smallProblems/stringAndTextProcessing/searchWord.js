6858df97d78e8fb2487b262f668722a625a72887
Demba Siby <dembasiby@gmail.com>
2024-11-13 02:31:43 +0000

    Completed first version of Search Word problem

---
+++ backend/js110/smallProblems/stringAndTextProcessing/searchWord.js
@@ -0,0 +1,18 @@
+function searchWord(word, str) {
+  let count = 0;
+  let startingIndex = 0;
+  str = str.toLowerCase();
+  word = word.toLowerCase();
+
+  while (str.indexOf(word, startingIndex) !== -1) {
+    count++;
+    startingIndex = str.indexOf(word, startingIndex) + 1;
+  }
+
+  return count;
+}
+
+const text =
+  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+
+console.log(searchWord("sed", text)); // 3
