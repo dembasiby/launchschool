19d8040928210afee13b90bf558f6d460da52173
Demba Siby <dembasiby@gmail.com>
2024-12-22 23:01:49 +0000

    Started Object prototypes practice problems

---
+++ backend/js120/lesson2_functions_objects_prototypes/practiceProblems.js
@@ -0,0 +1,29 @@
+function assignProperty(obj, prop, val) {
+  if (obj.hasOwnProperty(prop)) {
+    obj[prop] = val;
+    return;
+  }
+
+  while (obj && !obj.hasOwnProperty(prop)) {
+    obj = Object.getPrototypeOf(obj);
+
+    if (obj && obj.hasOwnProperty(prop)) {
+      obj[prop] = val;
+      return;
+    }
+  }
+}
+
+let fooA = { bar: 1 };
+let fooB = Object.create(fooA);
+let fooC = Object.create(fooB);
+
+assignProperty(fooC, "bar", 2);
+console.log(fooA.bar); // 2
+console.log(fooC.bar); // 2
+
+assignProperty(fooC, "qux", 3);
+console.log(fooA.qux); // undefined
+console.log(fooC.qux); // undefined
+console.log(fooA.hasOwnProperty("qux")); // false
+console.log(fooC.hasOwnProperty("qux")); // false
